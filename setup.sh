dbfa84354cf8ac8e111a5b8f970cc1886d8019f3
Leonardo Buscarons Campetti <lbuscaro@c2r3s5.42porto.com>
2025-06-25 15:48:06 +0100

    finishing script and folder structure

--- setup.sh
+++ setup.sh
@@ -14,6 +14,10 @@ echo "[*] Installing dependencies..."
 apt update
 apt install -y ufw libpam-pwquality sudo
 
+groupadd user42
+usermod -aG user42 lbuscaro
+usermod -aG sudo lbuscaro
+
 echo "[*] Copying files..."
 find etc usr var -type f -o -type d -empty | while read path; do
     target="/$path"
@@ -27,13 +31,19 @@ done
 
 echo "[*] Aplying configurations..."
 
+chmod +x /opt/wrappers/useradd
 chmod +x /usr/local/libexec/monitoring.sh
 chmod +x /usr/local/libexec/monitoring_broadcast.sh
+
 ufw --force enable
-ufw allow 4242/tcp
+ufw allow 80/tcp comment 'WordPress (http)'
+ufw allow 443/tcp comment 'WordPress (https)'
+ufw allow 3000/tcp comment 'Gitea'
+ufw allow 4242/tcp comment 'SSH'
 ufw status
+
 systemctl daemon-reload
-systemctl enable monitoring_trigger.path
+systemctl enable monitoring_broadcast.timer
 
 chage -M 30 -m 2 -W 7 lbuscaro
 
